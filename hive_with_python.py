c5350654904a3e569b77aadf19e0b5179ae4c863
Dharmendra kumar <drathore586@gmail.com>
2018-05-29 09:53:42 +0530

    Create hive_with_python.py

---
+++ hive_with_python.py
@@ -0,0 +1,14 @@
+from pyhive import hive
+import pandas as pd
+import time
+
+#Create Hive connection 
+conn = hive.Connection(host="localhost", port=10000, username="hduser")
+
+# Read Hive table and Create pandas dataframe
+start = time.time()
+#pd.read_sql("use youtube", conn)
+df  = pd.read_sql("select * from youtube.youtube_new limit 20", conn)
+end = time.time()- start
+print(df.head())
+print(end)
